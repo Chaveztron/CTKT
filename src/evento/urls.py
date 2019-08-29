d2924bff76eb965caac0595af15f815dbfe485fa
Jesus <jesus.chavez.caro@uabc.edu.mx>
2019-08-29 00:03:34 -0700

    link

--- src/evento/urls.py
+++ src/evento/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 urlpatterns = [
     
-    path('', comming, name='index2'),
-    path('root_jesus/', index, name='index'),
+
+    path('', index, name='index'),
     path('en/', indexen, name='englishp'),
     path('seguimiento/', programa, name='prog'),
     #path('log/', login, name='log'),
-    path('registrado/<id>/<nombre>/', satisfatorio, name='registrado' ),
+    path('registrado/<id>/<nombre>/', satisfatorio, name='registrado'),
     path('verificado/<id>/<nombre>/', verificar),
     path('tab/', registrados, name='tab'),
 
@@ -18,4 +18,5 @@ urlpatterns = [
 
     path('saludos/', HomeView.as_view()),
 
+    path('root_jesus/', comming, name='index2'),
 ]
